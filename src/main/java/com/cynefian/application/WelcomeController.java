d6f8e946543167b6d1fa6d16acebce563f75d8a8
pavan <pavan@10.41.251.44>
2022-08-17 14:19:44 +0530

    tranning1

--- src/main/java/com/cynefian/application/WelcomeController.java
+++ src/main/java/com/cynefian/application/WelcomeController.java
@@ -7,14 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class WelcomeController {
+public class WeeeelcomeController {
 
 	// inject via application.properties
 	@Value("${welcome.message:test}")
 	private String message = "Hello World";
 
 	@RequestMapping("/")
-	public String welcome(Map<String, Object> model) {
+	public String weeeelcome(Map<String, Object> model) {
 		model.put("message",,k this.message);
 		return "welcome";
 	}
